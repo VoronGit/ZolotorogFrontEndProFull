389139626068481473dc7b75331364696d7f66b1
VoronGit <evilvickers@gmail.com>
2022-12-21 14:22:07 +0200

    HomeWork 12 corrected!

--- HomeWork_12/script.js
+++ HomeWork_12/script.js
@@ -2,16 +2,12 @@ function removeElement(arr, item) {
     if (!Array.isArray(arr)) {
         console.log("Given item isn`t an Array!")
     } else {
-        for (let i = 0; i < arr.length; i++) {
-            if (arr[i] === item) {
-                arr.splice(i,1);
-            }
-        }
+        arr.splice(arr.indexOf(item),1);
     }
 }
 
 const array = [1, 2, 3, 4, 5, 6, 7];
 
 console.log(array);
-removeElement(array,2);
+removeElement(array,3);
 console.log(array);
