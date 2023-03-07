a42eff83ca6f78aa7ea665920592e9e74272fc82
VoronGit <evilvickers@gmail.com>
2023-03-07 19:51:26 +0200

    fix 07.03.2023

--- HomeWork_36/index.js
+++ HomeWork_36/index.js
@@ -1,37 +1,41 @@
 import { getUserByName, getUserInfractions } from './user-api.js';
 
-export function getRelevantInfractionReasons(username) {
-	return new Promise(function (resolve) {
-		getUserByName(username, function (user) {
-			getUserInfractions(user.id, function (result) {
-				// find most recent infraction
-				let foundIndexRecent = 0;
-				for (let i = 1; i < result.length; i++) {
-					if (result[i].id > result[foundIndexRecent].id) {
-						foundIndexRecent = i;
-					}
-				}
-				// find most recent infraction with most infraction points
-				let foundIndexWorst = 0;
-				for (let i = result.length - 1; i >= 0; i--) {
-					if (result[i].points > result[foundIndexWorst].points) {
-						foundIndexWorst = i;
-					}
-				}
 
-				//resolve({mostRecent, worst});
-				const finObj = {"mostRecent":replaceURLs(result[foundIndexRecent].reason), "worst":replaceURLs(result[foundIndexWorst].reason)};
-				resolve(finObj);
-			},function (error) {
-				resolve("no infractions found! Error: " + error);
-			})
-		},function () {
-			resolve("no user found! Error: " + error);
-		})
-	});
+export async function getRelevantInfractionReasons(username) {
+	// Get API info
+	const userG = await new Promise((resolveIns, rejectIns) => getUserByName(username, (user) => resolveIns(user), () => { rejectIns("User info not found!") }))
+		.then((user) => { return user })
+		.catch((error) => {
+			console.error(error);
+			return {};
+		});
+	const infG = userG.id ? await new Promise((resolveIns, rejectIns) => getUserInfractions(userG.id, (inf) => { resolveIns(inf) }, () => { rejectIns("User infractions not found!") }))
+		.then((inf) => { return inf })
+		.catch((error) => {
+			console.error(error);
+			return [];
+		}) : [];
+
+	if (infG && infG.length > 0) {
+		// find most recent and worst infraction
+		let foundIndexRecent = 0;
+		let foundIndexWorst = 0;
+		for (let i = 1; i < infG.length; i++) {
+			if (infG[i].id > infG[foundIndexRecent].id) {
+				foundIndexRecent = i;
+			}
+			if (infG[i].points > infG[foundIndexWorst].points) {
+				foundIndexWorst = i;
+			}
+		}
+		const finObj = { "mostRecent": replaceURLs(infG[foundIndexRecent].reason), "worst": replaceURLs(infG[foundIndexWorst].reason) };
+		return finObj;
+	} else {
+		return {};
+	}
 }
 
 // Universal function
 function replaceURLs(data) {
-	return data.replace(/\b(http)(s)?:\/\/\S+/,match => '<a href="' + match + '">' + match + '</a>');
+	return data.replace(/\b(http)(s)?:\/\/\S+/, match => `<a href="${match}">${match}</a>`);
 }
